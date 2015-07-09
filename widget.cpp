ba5ca9ef6eb671b81095d9aebe6cb90e37b476df
Viktor Fadeev <roninsc2@gmail.com>
2015-07-09 17:27:29 +0300

    fixed widget.cpp
    fixed widget.cpp

--- widget.cpp
+++ widget.cpp
@@ -21,8 +21,6 @@ Widget::~Widget()
 void Widget::paintEvent(QPaintEvent*)
 {
     QPainter painter(this);
-    QImage* img = new QImage("D:/wdjall/wdjmaster/data/images/navigation/crew.9.png");
-    painter.drawImage(0,0, *img,0,0, img->width(), img->height());
     try {
         ninepatch->Draw(painter, 20, 20, 107, 200);
     } catch (NinePatchException* e) {
